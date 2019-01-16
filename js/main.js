2710e962d499337d01cedf4c49d2edbd2e26287c
Wellington Costa <wcosta.ale@gmail.com>
2019-01-16 17:51:15 -0200

    comentarios, formatação

--- js/main.js
+++ js/main.js
@@ -22,14 +22,12 @@ function clickZoom(e) {
 
 function getLocate() { 
     function onLocationFound(e) {
-
         if(map.hasLayer(current_accuracy))
             map.removeLayer(current_accuracy);
         
         if(map.hasLayer(layer_origem))
             map.removeLayer(layer_origem);
 
-        // if position defined, then remove the existing position marker and accuracy circle from the map
         if (current_position) {
             map.removeLayer(current_position);
             map.removeLayer(current_accuracy);
@@ -41,11 +39,11 @@ function getLocate() {
         var greenIcon = L.icon({
             iconUrl: 'img/marker-icon.png',
             shadowUrl: 'img/marker-shadow.png',
-            iconSize:     [25, 41], // size of the icon
-            shadowSize:   [41, 41], // size of the shadow
-            iconAnchor:   [15, 35], // point of the icon which will correspond to marker's location
-            shadowAnchor: [15, 35],  // the same for the shadow
-            popupAnchor:  [-1, -30] // point from which the popup should open relative to the iconAnchor
+            iconSize:     [25, 41],
+            shadowSize:   [41, 41],
+            iconAnchor:   [15, 35],
+            shadowAnchor: [15, 35],
+            popupAnchor:  [-1, -30]
         });
         layer_local = L.marker(e.latlng, {icon: greenIcon}).addTo(map).bindPopup("Você está aquui");
 
@@ -67,11 +65,9 @@ function getLocate() {
     map.on('locationfound', onLocationFound);
     map.on('locationerror', onLocationError);
 
-    // wrap map.locate in a function    
     function locate() {
         map.locate({setView: true, maxZoom: 15});
     }
-
     setTimeout(function() { locate(); }, 300);
 }
 
@@ -79,7 +75,7 @@ function findPlace(lat, lng, dis) {
     $.getJSON( "data/base.json", function( data ) {
         $.each(data.places, function(index, obj) {
             var dif =  parseFloat(distance(lat, lng, obj.lat, obj.lng, 'K').toFixed(2));
-            
+            // dentro da distancia selecionada
             if(dif <= dis) { 
                 var place = {"dis": dif, "place": obj};
                 drawItem(place);
@@ -94,7 +90,6 @@ function drawItem(place) {
                     <p>${place.place.end}</p>
                     <p>Distância: ${place.dis} KM</p>
                 </div>`;
-
     $("#itens-place").append(html);
 }
 
@@ -108,14 +103,17 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 		var theta = lon1-lon2;
 		var radtheta = Math.PI * theta/180;
 		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-		if (dist > 1) {
+        
+        if (dist > 1) {
 			dist = 1;
-		}
+        }
+        
 		dist = Math.acos(dist);
 		dist = dist * 180/Math.PI;
 		dist = dist * 60 * 1.1515;
 		if (unit=="K") { dist = dist * 1.609344 }
-		if (unit=="N") { dist = dist * 0.8684 }
+        if (unit=="N") { dist = dist * 0.8684 }
+        
 		return dist;
 	}
 }
